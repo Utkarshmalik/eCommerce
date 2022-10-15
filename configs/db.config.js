d143e487250128623f8edd351d5fc472839b3780
utmalik <utmalik@amazon.com>
2022-10-15 15:51:22 +0530

    added db prod configs

--- configs/db.config.js
+++ configs/db.config.js
@@ -25,4 +25,17 @@ module.exports = {
             idle: 10000  // maximum time in ms that a connection can be idle before being released
         }
     },
+    production:{
+        HOST : "sql6.freemysqlhosting.net",
+        USER : "sql6526707",
+        PASSWORD : "rWBe32XM45",
+        DB : "sql6526707",
+        dialect : "mysql",
+        pool :{
+        max :5,
+        min :0,
+        acquire: 30000, //max time in ms that a pool will try to getconnection before throwing error
+        idle :10000 // maximum time in ms that a connection can be idlebefore being released
+        }
+       }
 }
